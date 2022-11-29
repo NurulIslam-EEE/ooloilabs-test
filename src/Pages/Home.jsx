219fd8e9ce0743ac3848f395a1138d50393681e9
NurulIslam-EEE <nurul.islam02310@gmail.com>
2022-11-29 22:52:00 +0600

    update home component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,58 +8,7 @@ import tab3 from "../table3Config.json"
 import "../styles/Home.css";
 
 function Home() {
-  const table1 = {
-    name: { 
-      show: true, 
-      sort: true },
-    joiningDate: { 
-      show: false, 
-      sort: false },
-    city: { 
-      show: false, 
-      sort: false },
-    role: { 
-      show: true, 
-      sort: false },
-    email: { 
-      show: true, 
-      sort: false },
-  };
-  const table2 = {
-    name: { 
-      show: false, 
-      sort: false 
-    },
-    joiningDate: { 
-      show: true, 
-      sort: true },
-    city: { 
-      show: false,
-       sort: false },
-    role: { 
-      show: true, 
-      sort: true },
-    email: { 
-      show: true, 
-      sort: false },
-  };
-  const table3 = {
-    name: { 
-      show: true, 
-      sort: false },
-    joiningDate: { 
-      show: true, 
-      sort: false },
-    city: { 
-      show: true, 
-      sort: true },
-    role: { 
-      show: true, 
-      sort: true },
-    email: { 
-      show: false, 
-      sort: false },
-  };
+
   return (
     <div className="table-container container">
       <h3>Part 01: All colum with sort</h3>
